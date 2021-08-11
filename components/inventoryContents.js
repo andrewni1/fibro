ada19078561e934ce8a14341c3ab8f247eb9e85b
unknown <ni95@purdue.edu>
2021-08-11 09:14:41 -0500

    Sales element

--- components/inventoryContents.js
+++ components/inventoryContents.js
@@ -452,39 +452,13 @@ export default function InventoryContents() {
                                             }}><FiEdit /></button>
                                             <Modal ariaHideApp={false} isOpen={editModalOpen} onRequestClose={() => setEditModalOpen(false)} className={style.modal}>
                                                 <div className={style.modal_content}>
-                                                    <form onSubmit={() => {
-                                                        firebase
-                                                            .firestore()
-                                                            .collection('items')
-                                                            .doc(item.id)
-                                                            .update({
-                                                                itemName: itemName,
-                                                                size: size,
-                                                                group: group,
-                                                                pricePaid: price,
-                                                        })
-                                                    }}>
-                                                        <input type="text" placeholder="Product Name" onChange={saveItemName} value={item.itemName} required />
-                                                        <input type="text" placeholder="Size" onChange={saveSize} value={item.size} required />
-                                                        <input type="text" placeholder="Group" onChange={saveGroup} value={item.group} required />
-                                                        <input type="number" step=".01" placeholder="Price Paid" onChange={savePrice} value={item.pricePaid} required />
-                                                        <button type="submit" >Edit Item</button>
-                                                    </form>
-                                                    <button onClick={() => setEditModalOpen(false)}>Close</button>
-                                                </div>
-                                            </Modal>
-                                            <button className={styles.sales_button} onClick={() => {
-                                                setSellModalOpen(true)
-                                            }}><MdAttachMoney /></button>
-                                            <Modal ariaHideApp={false} isOpen={sellModalOpen} onRequestClose={() => setSellModalOpen(false)} className={style.modal}>
-                                                <div className={style.modal_content}>
                                                     <form onSubmit={handleSubmit}>
                                                         <div>
-                                                            <div onClick={() => setSellModalOpen(false)} className={style.close_icon}>
+                                                            <div onClick={() => setEditModalOpen(false)} className={style.close_icon}>
                                                                 <FiX />
                                                             </div>
                                                             <div className={style.add_item}>
-                                                                <a>Item Seller</a>
+                                                                <a>Item Editer</a>
                                                             </div>
                                                         </div>
                                                         <a className={style.input_header}>PRODUCT NAME</a>
@@ -530,6 +504,93 @@ export default function InventoryContents() {
                                                             </div>
                                                             <div className={style.group_col}>
                                                                 <a className={style.input_header}>GROUP</a>
+                                                                <select className={style.group_box} onChange={saveGroup} defaultValue="" required>
+                                                                    <option disabled={true} value="" >Group</option>
+                                                                    <option onChange={saveGroup}>Sneakers</option>
+                                                                    <option onChange={saveGroup}>Streetwear</option>
+                                                                    <option onChange={saveGroup}>Electronics</option>
+                                                                    <option onChange={saveGroup}>Collectibles</option>
+                                                                    <option onChange={saveGroup}>Other</option>
+                                                                </select>
+                                                            </div>
+                                                        </div>
+                                                        <div className={style.buttons}>
+                                                            <button className={style.add_box} type="submit" onSubmit={() => {
+                                                                firebase
+                                                                    .firestore()
+                                                                    .collection('items')
+                                                                    .doc(item.id)
+                                                                    .update({
+                                                                        itemName: itemName,
+                                                                        pricePaid: pricePaid,
+                                                                        size: size,
+                                                                        group: group,
+                                                                    })
+                                                                    .then(() => {
+                                                                        console.log(item.id)
+                                                                    })
+                                                            }}>Edit</button>
+                                                        </div>
+                                                    </form>
+                                                </div>
+                                            </Modal>
+                                            <button className={styles.sales_button} onClick={() => {
+                                                setSellModalOpen(true)
+                                            }}><MdAttachMoney /></button>
+                                            <Modal ariaHideApp={false} isOpen={sellModalOpen} onRequestClose={() => setSellModalOpen(false)} className={style.modal}>
+                                                <div className={style.modal_content}>
+                                                    <form onSubmit={handleSubmit}>
+                                                        <div>
+                                                            <div onClick={() => setSellModalOpen(false)} className={style.close_icon}>
+                                                                <FiX />
+                                                            </div>
+                                                            <div className={style.add_item}>
+                                                                <a>Item Seller</a>
+                                                            </div>
+                                                        </div>
+                                                        <a className={style.input_header}>PRODUCT NAME</a>
+                                                        <input className={style.name_box} type="text" placeholder="Product name" onChange={saveItemName} required/>
+                                                        <div className={style.input_grid}>
+                                                            <div className={style.price_col}>
+                                                                <a className={style.input_header}>PURCHASE PRICE</a>
+                                                                <input type="number" step=".01" className={style.price_box} placeholder="Price" onChange={savePrice} required />
+                                                            </div>
+                                                            <div className={style.size_col}>
+                                                                <a className={style.input_header}>SIZE</a>
+                                                                <select className={style.size_box} onChange={saveSize} required>
+                                                                    <option disabled={true} value="">Size</option>
+                                                                    <option onChange={saveSize}>N/A</option>
+                                                                    <option onChange={saveSize}>XS</option>
+                                                                    <option onChange={saveSize}>S</option>
+                                                                    <option onChange={saveSize}>M</option>
+                                                                    <option onChange={saveSize}>L</option>
+                                                                    <option onChange={saveSize}>XL</option>
+                                                                    <option onChange={saveSize}>XXL</option>
+                                                                    <option onChange={saveSize}>3.5</option>
+                                                                    <option onChange={saveSize}>4</option>
+                                                                    <option onChange={saveSize}>4.5</option>
+                                                                    <option onChange={saveSize}>5</option>
+                                                                    <option onChange={saveSize}>5.5</option>
+                                                                    <option onChange={saveSize}>6</option>
+                                                                    <option onChange={saveSize}>6.5</option>
+                                                                    <option onChange={saveSize}>7.5</option>
+                                                                    <option onChange={saveSize}>8</option>
+                                                                    <option onChange={saveSize}>8.5</option>
+                                                                    <option onChange={saveSize}>9</option>
+                                                                    <option onChange={saveSize}>9.5</option>
+                                                                    <option onChange={saveSize}>10</option>
+                                                                    <option onChange={saveSize}>10.5</option>
+                                                                    <option onChange={saveSize}>11</option>
+                                                                    <option onChange={saveSize}>11.5</option>
+                                                                    <option onChange={saveSize}>12</option>
+                                                                    <option onChange={saveSize}>12.5</option>
+                                                                    <option onChange={saveSize}>13</option>
+                                                                    <option onChange={saveSize}>13.5</option>
+                                                                    <option onChange={saveSize}>14</option>
+                                                                </select>
+                                                            </div>
+                                                            <div className={style.group_col}>
+                                                                <a className={style.input_header}>GROUP</a>
                                                                 <select className={style.group_box} onChange={saveGroup} defaultValue={item.id.group} required>
                                                                     <option disabled={true} value="">Group</option>
                                                                     <option onChange={saveGroup}>Sneakers</option>
@@ -542,12 +603,13 @@ export default function InventoryContents() {
                                                         </div>
                                                         <div className={style.buttons}>
                                                             <button className={style.add_box} type="submit" onClick={() => {
-                                                                //Needs to change sold value to true
                                                                 firebase
                                                                     .firestore()
                                                                     .collection('items')
                                                                     .doc(item.id)
-                                                                    .delete()
+                                                                    .update({
+                                                                        sold: true,
+                                                                    })
                                                                     .then(() => {
                                                                         console.log(item.id)
                                                                     })
